39b5cd5e411a123d6802a045d9fb4ca1c13ecad1
Pedram <tadayoni@gmail.com>
2019-10-14 21:58:52 -0700

    Added tests

--- src/test/java/edu/udacity/java/nano/chat/WebSocketChatApplicationTest.java
+++ src/test/java/edu/udacity/java/nano/chat/WebSocketChatApplicationTest.java
@@ -1,4 +1,54 @@
 package edu.udacity.java.nano.chat;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class WebSocketChatApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Message message;
+
+    @Test
+    public void login() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(view().name("/login"));
+    }
+
+    @Test
+    public void userJoin() throws Exception {
+
+        this.mockMvc.perform(get("/index?username=john")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("john")));
+
+    }
+
+    @Test
+    public void chat() throws Exception {
+
+        this.mockMvc.perform(get("/index?username=john")).andDo(print()).andExpect(status().isOk())
+                .andExpect(view().name("chat"));
+    }
+
+    @Test
+    public void leave() throws Exception {
+        this.mockMvc.perform(get("/index?username=john")).andDo(print()).andExpect(status().isOk())
+                .andExpect(view().name("chat"));
+
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(view().name("/login"));
+    }
+
 }
